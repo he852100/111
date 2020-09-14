9a140c0647db29bf5f99d9701f72b1bdb3603fd4
he852100 <he852100@huojia1.win>
2020-09-14 21:59:00 +0800

    Add files via upload

---
+++ camtasia/1/scripts/config_xml.js
@@ -0,0 +1,50 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2020-09-14 08:33:51 下午" dc:source="Camtasia,9.1.4,enu" dc:title="x264" tscDM:firstFrame="x264_First_Frame.png" tscDM:originId="A7DB1E74-EB60-4A47-82B5-5090C2830D89" tscDM:project="x264">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="109100"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="1080" stDim:w="1920"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="EB8CD59A-2F2E-4AC9-9E74-D53DE05A3D5F" tscIQ:authoredEmail="b3NS9c6RiUCRhWZCkxcTbpOPDPKvX7B1nVWECcdeDAaum3lunGNETFatWKWPaAqmj9/OYCC2wdAc\
+fbL93A8w3L6Ebx8NBdn8UAyG9896ifsdhmR3+l36Yi1RIDY0q0damgU2qL/Ybn1LVbZKcL0e7wPO\
+jWCoI2AmWtrSor0w2wPraS6eAnTUuJw1QYqDvjdabvctl2sgOtiNqa2q/ILL79M+xKyvr+S0FXRD\
+7dKNiWDCv3rHIBmHgWC+Kf5Z6bmhueH+rbvnShxtqjgjhkpwfU3Ljch/GXgqicJw2CNZOBTPll39\
+LgMZV1GxQ9U++3ErJzmahu2NNzmPvnYxYf3UdQ==" tscIQ:requireUserId="0" tscIQ:locale="en-US" tscIQ:reportMethod="NONE" tscIQ:allowSkipQuiz="0" tscIQ:clientId="1CDF22D874AC443FBBB68075B4AE31CD" tscIQ:hideReplay="0" tscIQ:quizHash="54eccb82886e2fad7608ed8482758c28">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="57167" tscIQ:feedback="1" tscIQ:questionSetName="测验 3"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="FITB" tscIQ:id="0"><tscIQ:question>刚才搜索了什么？</tscIQ:question><tscIQ:correctAnswer/><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>x264vfw</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct" tscIQ:location="https://sourceforge.net/projects/x264vfw/"><xmpDM:name><rdf:Alt><rdf:li>恭喜你都会抢答了。</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect" tscIQ:jumpToTime="0"><xmpDM:name><rdf:Alt><rdf:li>笨</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="1"><tscIQ:question>预设问题文本</tscIQ:question><tscIQ:correctAnswer>1</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>O</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>X</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li></rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect"><xmpDM:name><rdf:Alt><rdf:li></rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="上一个"/><rdf:li xmpDM:name="txtNext" xmpDM:value="下一步"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="开始测验"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="提交答案"/><rdf:li xmpDM:name="txtReview" xmpDM:value="重播最后一节"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="查看答案"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="继续"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="right"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="x264.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="x264_First_Frame.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
